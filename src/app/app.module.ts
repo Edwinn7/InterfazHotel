8dce4aa4ec0470648991b2757cc122c6c9fb00b5
paalmorales27 <paalmorales27@gmail.com>
2022-11-11 08:01:43 -0600

    traer los datos del api y creacion de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FormreservaComponent } from './formreserva/formreserva.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { DescripcionComponent } from './descripcion/descripcion.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    FormreservaComponent,
+    HabitacionesComponent,
+    DescripcionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formreserva/formreserva.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormreservaComponent } from './formreserva.component';
+
+describe('FormreservaComponent', () => {
+  let component: FormreservaComponent;
+  let fixture: ComponentFixture<FormreservaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormreservaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FormreservaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
